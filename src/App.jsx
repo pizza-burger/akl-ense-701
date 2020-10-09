2398a1254ebc631196654ba21bbd735da1bbad89
NickW1412 <69229004+NickW1412@users.noreply.github.com>
2020-10-09 15:32:00 +1300

    Revert "Update App.jsx"
    This reverts commit 2f96c0372063768f6739cd3ea5bf01da2a8e4505.

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { Typography, Grid, TextField } from "@material-ui/core";
-const App = (props) => {
-  const [state, setState] = useState("");
-  const [counter, setCounter] = useState(0);
+import React from "react";
 
-  useEffect(() => {
-    setCounter(state.length);
-  }, [state]);
-
-  return (
-    <>
-      <Grid container spacing={2}>
-        <Grid item xs></Grid>
-        <Grid
-          item
-          xs
-          style={{ marginRight: "auto", marginLeft: "auto", display: "flex" }}
-        >
-          <Typography
-            color="primary"
-            style={{ textAlign: "center" }}
-            variant="h2"
-          >
-            Hello world
-          </Typography>
-        </Grid>
-        <Grid item xs></Grid>
-        <Grid item xs={12}>
-          <Grid container spacing={2}>
-            <Grid item xs={3}>
-              <TextField
-                label="Get word count here"
-                onChange={(e) => {
-                  setState(e.target.value);
-                }}
-              />
-            </Grid>
-            <Grid item xs={4} style={{ display: "flex" }}>
-              <Typography style={{ margin: "auto" }} variant="body2">
-                If you are seeing word count changing, react is enabled {`=>`}
-                word count: {counter}
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-    </>
-  );
-};
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        My App
+      </div>
+    )
+  }
+}
 
 export default App;
