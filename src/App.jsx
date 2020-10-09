030d8674f5f40ed958e84669834caf3c149a68cd
NickW1412 <69229004+NickW1412@users.noreply.github.com>
2020-10-09 15:33:05 +1300

    change app root to functional compone, test material-ui, with react hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,50 @@
-import React from "react";
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        My App
-      </div>
-    )
-  }
-}
+import React, { useState, useEffect } from "react";
+import { Typography, Grid, TextField } from "@material-ui/core";
+const App = (props) => {
+  const [state, setState] = useState("");
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    setCounter(state.length);
+  }, [state]);
+  return (
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs></Grid>
+        <Grid
+          item
+          xs
+          style={{ marginRight: "auto", marginLeft: "auto", display: "flex" }}
+        >
+          <Typography
+            color="primary"
+            style={{ textAlign: "center" }}
+            variant="h2"
+          >
+            Hello world
+          </Typography>
+        </Grid>
+        <Grid item xs></Grid>
+        <Grid item xs={12}>
+          <Grid container spacing={2}>
+            <Grid item xs={3}>
+              <TextField
+                label="Get word count here"
+                onChange={(e) => {
+                  setState(e.target.value);
+                }}
+              />
+            </Grid>
+            <Grid item xs={4} style={{ display: "flex" }}>
+              <Typography style={{ margin: "auto" }} variant="body2">
+                If you are seeing word count changing, react is enabled {`=>`}
+                word count: {counter}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
 export default App;
