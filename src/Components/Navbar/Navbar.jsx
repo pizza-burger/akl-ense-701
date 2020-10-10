188296d7084944264ee3a6612ff7f7ed8d5aed1f
NickW1412 <69229004+NickW1412@users.noreply.github.com>
2020-10-10 17:02:07 +1300

    mark nav bar be fixed on top

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import { AppBar, Toolbar, IconButton,Typography } from "@material-ui/core";
 const Navbar = (props) => {
 
 
-    return <AppBar position="sticky">
+    return <AppBar position="fixed" style={{top:"0px"}}>
         <Toolbar >
             <Typography edge="end" variant="h6" style={{
                 color:"#FFF"
