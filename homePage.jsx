154262a62c307ce48c0bfd6bf19f0382cc96062e
Kooshan <55038456+akay4ty7@users.noreply.github.com>
2020-10-13 17:19:18 +1300

    Basic Home Page
    Created a basic homepage.
    Code Reference: https://reactrouter.com/web/example/basic

---
+++ homePage.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// This site has 3 pages, all of which are rendered
+// dynamically in the browser (not server rendered).
+//
+// Although the page does not ever refresh, notice how
+// React Router keeps the URL up to date as you navigate
+// through the site. This preserves the browser history,
+// making sure things like the back button and bookmarks
+// work properly.
+
+export default function BasicExample() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Searcher</Link>
+          </li>
+          <li>
+            <Link to="/about">Submitter</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Moderator</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Searcher />
+          </Route>
+          <Route path="/Submitter">
+            <Submitter />
+          </Route>
+          <Route path="/Moderator">
+            <Moderator />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+// You can think of these components as "pages"
+// in your app.
+
+function Searcher() {
+  return (
+    <div>
+      <h2>Searcher</h2>
+    </div>
+  );
+}
+
+function Submitter() {
+  return (
+    <div>
+      <h2>Submitter</h2>
+    </div>
+  );
+}
+
+function Moderator() {
+  return (
+    <div>
+      <h2>Moderator</h2>
+    </div>
+  );
+}
