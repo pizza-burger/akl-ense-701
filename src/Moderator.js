2dd89fe2fbfa3d5a113e55bf4fb8dc2a4540386c
19Maaka <62680289+19Maaka@users.noreply.github.com>
2020-10-14 10:38:33 +0800

    add moderator page

--- src/Moderator.js
+++ src/Moderator.js
@@ -6,10 +6,38 @@ class Moderator extends React.Component {
       <div className="Moderator">
         <h1>Submit new Study</h1>
         <form>
-          <label for="title">Title:</label><br />
-          <input type="text" id="title" name="title"></input>
-          <label for="author">Author:</label><br />
-          <input type="text" id="author" name="author"></input>
+        
+          <label for="title">Title:</label>
+          <input type="text" id="title" name="title"></input><br /><br />
+          <label for="Denied">Denied:</label>
+          <div class="dropdown">
+            <ul class="dropdown-menu">
+              <li><a href="#">case1</a></li>
+              <li><a href="#">case2</a></li>
+              <li><a href="#">case3</a></li>
+            </ul>
+          </div>
+          <button type="up">^</button>
+          <button type="down">V</button><br />
+          <label for="Pending">Pending:</label>
+          <div class="dropdown">
+            <ul class="dropdown-menu">
+              <li><a href="#">case1</a></li>
+              <li><a href="#">case2</a></li>
+              <li><a href="#">case3</a></li>
+            </ul>
+          </div>
+          <button type="up">^</button>
+          <button type="down">V</button><br />
+          <label for="Approved">Approved:</label>
+          <div class="dropdown">
+            <ul class="dropdown-menu">
+              <li><a href="#">case1</a></li>
+              <li><a href="#">case2</a></li>
+              <li><a href="#">case3</a></li>
+            </ul>
+          </div><br />
+          <button type="conform">conform</button>
         </form>
       </div>
     );
