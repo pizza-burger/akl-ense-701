a994516c780cd543df90c404739bc5b4222be9d1
19Maaka <62680289+19Maaka@users.noreply.github.com>
2020-10-15 19:52:45 +0800

    mongo db

--- src/Home.js
+++ src/Home.js
@@ -9,10 +9,47 @@ import SubmitStudy from "./pages/SubmitStudy.jsx";
 import Moderator from "./pages/Moderator.jsx";
 import Searcher from "./pages/Searcher.jsx";
 
+
+/**const {MongoClient} = require('mongodb');
+
+async function createDB(){
+  const uri = "mongodb+srv://maaka:123@cluster0.uontj.mongodb.net/Cluster0?retryWrites=true&w=majority";
+
+  const client = new MongoClient(uri);
+
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+
+      // Make the appropriate DB calls
+      await  listDatabases(client);
+
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+}
+
+//createDB().catch(console.error);
+
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+
+  Teststring = databasesList.testDB.find({"TestName": "Maaka"});
+};*/
+
 function Home() {
+  //createDB();
+  console.log('hi');
   return (
     <Router>
       <h1>SEER MENU</h1>
+      <p id="demo"></p>
+        <script>
+          const Tstring = "HI";
+          document.getElementById("demo").innerHTML = Tstring;
+        </script>
       <div>
         <ul>
           <li>
